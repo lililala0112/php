65d2bada3ce4e2e91ae031ba36dfe89dfa360472
lililala0112 <lililala0112@gmail.com>
2016-07-05 15:48:45 +0800

    fopen

---
+++ fopen.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<body>
+    
+    
+  <?php
+  
+  //fopen 用法 http://php.net/function.fopen
+  //開新檔案
+  $fh = fopen('fopen_a.php', 'a');
+  //寫入資料
+  fwrite($fh, '<h1>Hello world!</h1>');
+  fclose($fh);
+  //刪除檔案
+  unlink('fopen_a.php');
+
+  ?>
+
+</body>
+</html>
+
