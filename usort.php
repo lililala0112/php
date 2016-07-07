a075d11d25f160669cdff000de313db5db8e3531
lililala0112 <lililala0112@gmail.com>
2016-07-07 16:22:55 +0800

    usort

---
+++ usort.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<?php 
+   
+   
+   $arr = array( '台北市','桃園市','新竹市','彰化市','台中市','桃園市','新竹市', '台北市');	
+
+  //array_unique 刪除相同的陣列值
+
+  var_dump( array_unique($arr) );
+
+   // 自訂排序
+
+   function cmp($a,$b){
+
+       if($a == $b){
+       		return 0;
+       }
+       
+       return ($a<$b)?-1:1;
+   }
+   $aNum = array(2,2,5,6,1);
+   usort($aNum,'cmp');
+   var_dump($aNum);
+
+   //關鍵字搜尋
+   //in_array(value) bloon
+   //array_search( value ) -->回傳其key
+
+   $array = array(0 => 'blue', 1 => 'red', 2 => 'green', 3 => 'black', 4 => 'black');
+
+   echo array_search('black', $array, true);
+
+   $newarray = array();
+   foreach ($array as $key => $value) {
+   	   if( $array[$key] == "black"){
+   	   		array_push( $newarray,$array[$key] );
+   	   }
+   }
+   var_dump($newarray);
+
+
+   //array_diff
+   //比對$test1,$test2 以test1為依準 把$test2沒有的值印出來
+
+   $test1 = array(1,2,3,4,5);
+   $test2 = array(4,2,5,6,7);
+   $var = array_diff($test1,$test2);
+   print_r($var);//Array ( [0] => 1 [2] => 3 )
+
+   //array_diff_assoc()
+   //比對$test1,$test2 以test1為依準 對索引$test2沒有的值印出來
+
+   $test1 = array(1,2,3,4,5);
+   $test2 = array(4,2,5,6,7);
+   $var = array_diff_assoc($test1,$test2);
+   print_r($var);//Array ( [0] => 1 [2] => 3 [3] => 4 [4] => 5 )
+  
+
+ ?>
+	
+</body>
+</html>
